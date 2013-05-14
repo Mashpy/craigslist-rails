f2929a64bc12548b101f2e1f32e0dbb5df2d45ec
Angie Bui <angiebui@gmail.com>
2013-05-13 20:01:47 -0700

    before_filter for posts and flash notice

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,16 @@
 class PostsController < ApplicationController
   before_filter :get_category, only: [:new, :edit]
-
+  before_filter :get_post, only: [:show, :edit, :update, :destroy]
+  
   def get_category
     @category = Category.find(params[:category_id])
   end
 
-  def show
+  def get_post
     @post = Post.find(params[:id])
+  end
+
+  def show
   end
 
   def new
@@ -19,13 +23,16 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     @post.update_attributes(params[:post])
     redirect_to category_post_path(@post.category, @post)
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_url, notice: "Deleted!"
   end
 end
 
